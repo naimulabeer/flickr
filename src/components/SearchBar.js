81b501176f1706d646717f96ba0a5dc3e711c603
naimulabeer <naimul.h.abeer@gmail.com>
2023-05-24 00:12:52 +0600

    handling form submission

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,13 @@
 function SearchBar({ onSubmit }) {
-  const handleClick = () => {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
     onSubmit("cars");
   };
   return (
     <div>
-      <input />
-      <button onClick={handleClick}>Click!</button>
+      <form onSubmit={handleFormSubmit}>
+        <input />
+      </form>
     </div>
   );
 }
